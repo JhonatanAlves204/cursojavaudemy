f7cd1e68cf22eb33acc77a8e23ad320ab2ca5ea3
Jhon Alves <jhonatanalvessilva204@gmail.com>
2026-03-25 13:09:30 -0300

    Atualizado classes product e enter product

--- src/OrientacaoObjeto/EnterProduct.java
+++ src/OrientacaoObjeto/EnterProduct.java
@@ -11,29 +11,28 @@ public class EnterProduct {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.println("Enter product data: ");
         System.out.println("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.println("Price: ");
-        product.price = sc.nextDouble();
-        System.out.println("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        double price = sc.nextDouble();
+        Product product = new Product(name, price);
 
-        System.out.println("Product data: " + product.name + ", $ " + product.price + ", " + product.quantity + " units, Total: $ " + product.TotalValueInStock());
+        product.setName("Computer");
+        System.out.println("Updated name: " + product.getName());
+
         System.out.println("Enter the number of products to be added in stock: ");
         int quantityToAdd = sc.nextInt();
         product.AddProducts(quantityToAdd);
         sc.nextLine();
 
-        System.out.println("Updated data: " + product.name + ", $ " + product.price + ", " + product.quantity + " units, Total: $ " + product.TotalValueInStock());
+        System.out.println("Updated data: " + product.getName() + ", $ " + product.getPrice() + ", " + product.getQuantity() + " units, Total: $ " + product.TotalValueInStock());
         System.out.println("Enter the number of products to be removed in stock: ");
         int quantityToRemove = sc.nextInt();
         product.RemoveProducts(quantityToRemove);
         sc.nextLine();
 
-        System.out.println("Updated data: " + product.name + ", $ " + product.price + ", " + product.quantity + " units, Total: $ " + product.TotalValueInStock());
+        System.out.println("Updated data: " + product.getName() + ", $ " + product.getPrice() + ", " + product.getQuantity() + " units, Total: $ " + product.TotalValueInStock());
 
 
 
